a616d47b7e47369bfe6b458d291bf0b8110f7d7b
e <lwl5633@gmail.com>
2017-08-18 16:08:23 +0800

    2018.8.18

---
+++ Chaper two/priticts/2.1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+    printf("Robot_LI\n");
+    printf("Robot\n");
+    printf("LI\n");
+    printf("Robot");
+    printf("LI");
+    getchar();
+    return 0;
+
+}
---
+++ Chaper two/priticts/2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    x = 10;
+    y = 5;
+    y= x + y;
+    x= x*y;
+    printf("%d %d",x,y );
+    getchar();
+    return 0;
+}
